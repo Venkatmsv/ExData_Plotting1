4782274c72dac7ab2410700267c19021d8f23bb5
Venkat M <Venkatesan.Murugappan@pearson.com>
2016-03-23 00:21:18 -0400

    updated the code comment

--- LoadPlotData.r
+++ LoadPlotData.r
@@ -10,14 +10,11 @@ energyUsageData[,dateTimeCols] <- lapply(dateTimeCols,function(x) as.character(e
 otherCols <- c("Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 energyUsageData[,otherCols] <- lapply(otherCols,function(x) as.numeric(as.character(energyUsageData[,x])))
 
-
-#convert the Time column as time type from the factor type
+#convert the Time column as time type from the character type
 energyUsageData$Time <- strptime(paste(energyUsageData$Date,energyUsageData$Time),format="%d/%m/%Y %H:%M:%S")
 
-#convert column Date as date type from factor type
+#convert column Date as date type from character type
 energyUsageData$Date <- as.Date(energyUsageData$Date,format="%d/%m/%Y")
-
-
 
 #subset the required data for plotting as per the given requirement
 dateFilter <- as.Date(c("2007-02-01","2007-02-02"),"%Y-%m-%d")
